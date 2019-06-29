3d49a0f952a33d088ae055898e68f05e27ef850d
youming <1570819807@qq.com>
2019-06-29 20:25:37 +0800

    添加数据包封装模块(数据包骨架类、转义、还原、crc16等)

---
+++ src/main/java/Blove/Packet/impl/PacketBaseImpl.java
@@ -0,0 +1,12 @@
+package Blove.Packet.impl;
+
+/*
+ * @Time    : 2019/6/29 6:28 PM
+ * @Author  : YouMing
+ * @Email   : myoueva@gmail.com
+ * @File    : PacketBaseImpl.java
+ * @Software: IntelliJ IDEA
+ */
+public interface PacketBaseImpl {
+
+}
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,9 @@
 import Blove.Packet.Enums.RpcBeginAndEndSignal;
 import Blove.Packet.Enums.RpcFpsType;
+import Blove.Util.CRCUtil;
+import sun.misc.CRC16;
+
+import java.util.zip.CRC32;
 
 /*
  * @Time    : 2019/6/28 5:09 PM
@@ -12,5 +16,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println(RpcBeginAndEndSignal.END_FRAME.getCode());
         System.out.println(RpcFpsType.BAD_REQUEST.getCode());
+        System.out.println(CRCUtil.getCRC("ss".getBytes()));
     }
 }
