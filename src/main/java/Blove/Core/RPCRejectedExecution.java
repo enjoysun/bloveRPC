d2b734ef127432530f4a0bbba7996ef9e46fccea
youming <1570819807@qq.com>
2019-07-01 14:19:39 +0800

    业务逻辑处理线程池

---
+++ src/main/java/Blove/Core/RPCRejectedExecution.java
@@ -0,0 +1,28 @@
+package Blove.Core;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/*
+ * @Time    : 2019/7/1 1:30 PM
+ * @Author  : YouMing
+ * @Email   : myoueva@gmail.com
+ * @File    : RPCRejectedExecution.java
+ * @Software: IntelliJ IDEA
+ */
+public class RPCRejectedExecution implements RejectedExecutionHandler {
+
+    /**
+     * @Author myou<myoueva@gmail.com>
+     * @Description 自定义rpc线程池饱和策略
+     * @Date 1:30 PM 2019/7/1
+     * @Param
+     * @return
+     **/
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println("线程池"+executor.toString()+"满载处理-----");
+        new ThreadPoolExecutor.DiscardOldestPolicy().rejectedExecution(r, executor);
+    }
+}
---
+++ src/main/java/Blove/Core/RPCThreadPool.java
@@ -0,0 +1,31 @@
+package Blove.Core;
+
+import java.util.concurrent.*;
+
+/*
+ * @Time    : 2019/7/1 1:08 PM
+ * @Author  : YouMing
+ * @Email   : myoueva@gmail.com
+ * @File    : RPCThreadPool.java
+ * @Software: IntelliJ IDEA
+ */
+public class RPCThreadPool {
+    /**
+     * @Author myou<myoueva@gmail.com>
+     * @Description 自定义业务处理线程池，防止netty的handler处理复杂业务逻辑阻塞影响io返回
+     * @Date 1:08 PM 2019/7/1
+     * @Param
+     * @return
+     **/
+    public static ExecutorService getExecutor(int threads, int queues){
+        return new ThreadPoolExecutor(
+                threads,
+                Runtime.getRuntime().availableProcessors()*2,
+                0,
+                TimeUnit.MILLISECONDS,
+                queues==0?new SynchronousQueue<Runnable>():(queues<0?new LinkedBlockingQueue<Runnable>():new LinkedBlockingQueue<Runnable>(queues)),
+                new RPCLayerThreadFactory("RPCPool", true),
+                new RPCRejectedExecution()
+                );
+    }
+}
